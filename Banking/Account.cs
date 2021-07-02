d721ad8444e564a9e3753ac5eebe29993be69764
Seth-Race <sethrace@gmail.com>
2021-07-02 15:34:20 -0400

    Init commit

---
+++ Banking/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Balance { get; private set; } = 0;
+
+
+        public void Deposit(decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be greater than zero.");
+            } else
+            Balance += Amount;
+        }
+        public void Withdraw(decimal Amount) {
+            if (Amount <= 0) {
+                throw new Exception("Amount must be greater than zero.");
+            } if (Amount > Balance) {
+                Console.WriteLine("Insufficient funds.");
+                return;
+            }
+            else
+                Balance -= Amount;
+        }
+
+    }
+}
---
+++ Banking/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Banking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Banking App!");
+            var acct1 = new Account() {
+                Id = 1234, Description = "First Account"
+            };
+            acct1.Deposit(1000);
+            acct1.Withdraw(300);
+            Console.WriteLine($"Balance: {acct1.Balance}");
+            acct1.Deposit(200);
+            Console.WriteLine($"Balance: {acct1.Balance}");
+
+
+
+
+
+            Console.WriteLine("Thanks for using the app!");
+        }
+    }
+}
